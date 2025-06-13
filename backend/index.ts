907946774583f8f28a7adec21cd9dadaf5e77f25
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-06-13 19:46:16 -0300

    form working

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(3001, () => {
   console.log("Server is running on port 3000");
@@ -10,6 +14,16 @@ app.get("/error", (req, res) => {
   res.status(500).json({ message: "error" });
 });
 
+const transactions = [{ transaction: "Test 1", amount: 5 }];
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello" });
+  res.json({ transactions });
+});
+
+app.post("/", (req, res) => {
+  console.log({ test: req.body });
+  const { name, amount } = req.body;
+  transactions.push({ transaction: name, amount });
+  console.log({ transactions });
+  res.json({ message: "success" });
 });
